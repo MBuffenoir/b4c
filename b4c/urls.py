cfd17b2a1db0a825e54b53c909d2838e43707332
lalu <mathieu@digithink.ch>
2015-11-21 13:37:02 +0100

    renaming to b4c

--- b4c/urls.py
+++ b4c/urls.py
@@ -1,4 +1,4 @@
-"""webapp URL Configuration
+"""b4c URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
