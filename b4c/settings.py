2aaa352f37bf61e8bf550e6f067c6085509152cb
lalu <mathieu@digithink.ch>
2015-12-05 02:57:04 +0100

    Added support for swarm network in settings.py

--- b4c/settings.py
+++ b4c/settings.py
@@ -88,15 +88,26 @@ WSGI_APPLICATION = 'b4c.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+if os.getenv('POSTGRES_URI'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'HOST': os.getenv('POSTGRES_URI'),
+            'PORT': 5432, 
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'HOST': 'db',
+            'PORT': 5432,
+        }
+    }
 
 # Django All auth settings
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
